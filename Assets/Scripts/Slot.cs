c7459caadda41d4906161d4fa9e1feef4bc380dc
kazz62 <theo.delbarre1@hotmail.fr>
2024-01-20 16:11:53 +0100

    feature_inventory_tooltip

---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public ItemData item;
+    public Image itemVisual;
+    public void OnPointerEnter(PointerEventData eventDate)
+    {
+        if(item)
+        {
+            TooltipSystem.instance.Show(item.description, item.name);
+        }
+    }
+    public void OnPointerExit(PointerEventData eventDate)
+    {
+        TooltipSystem.instance.Hide();
+    }
+}
